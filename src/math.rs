b1ab84c45b8c40429ae4266d9344661cdcbcb84f
sandalbanditten <sandalbanditten@tutanota.com>
2022-03-29 13:56:34 +0200

    A little collection of math functions, for now just a mapping function

---
+++ src/math.rs
@@ -0,0 +1,5 @@
+// Map a number within a range, to within another range
+pub fn map(num: f32, start_one: f32, stop_one: f32, start_two: f32, stop_two: f32) -> f32 {
+    // Return the mapped result
+    (num - start_one) / (stop_one - start_one) * (stop_two - start_two) + start_two
+}
