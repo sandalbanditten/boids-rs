f82c3142d245f4dc6815790ad4cbeb951ad0189d
sandalbanditten <sandalbanditten@tutanota.com>
2022-05-02 15:18:51 +0200

    Putting the draw struct on the model now, and added INITIAL_BOIDS const

--- src/model.rs
+++ src/model.rs
@@ -3,38 +3,41 @@ use crate::flock::Flock;
 use crate::keys::{key_pressed, key_released, Keybinds};
 use crate::view;
 use crate::window::window_resized;
-use nannou::prelude::{App, Rect};
+use nannou::prelude::{App, Draw, Rect};
 
-// TODO: Make these fields private, and make some methods for interacting with them
+const INITIAL_BOIDS: usize = 1024;
+
 pub struct Model {
     pub flock: Vec<Boid>,
     pub keybinds: Keybinds,
     pub win_rect: Rect,
+    pub draw: Draw,
 }
 
 impl Model {
     // Our constructor
-    pub fn new(app: &App) -> Model {
+    pub fn new(app: &App) -> Self {
         // Creating the window
         let _window = app
             .new_window()
-            .title("1024 boids!")
+            .title(format!("{} boids!", INITIAL_BOIDS))
             // Functions to call at certain event
             .key_pressed(key_pressed)
             .key_released(key_released)
             .resized(window_resized)
             .view(view)
             .build()
-            .expect("Unable to build the window");
+            .expect("Unable to build the app");
 
         // The window rect
         let win_rect = app.window_rect();
 
         // Our model is the state of our application, which can be accessed from all functions
         Model {
-            flock: Flock::new_flock(win_rect, 1024),
+            flock: Flock::new_flock(win_rect, INITIAL_BOIDS),
             keybinds: Keybinds::new(),
             win_rect,
+            draw: app.draw(),
         }
     }
 }
