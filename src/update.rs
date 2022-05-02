dd432341185ae8eb95c9df583ed861fe3ee743fd
sandalbanditten <sandalbanditten@tutanota.com>
2022-05-02 15:19:25 +0200

    Changed from draw to model.draw, and now using rayons par_iter_mut()

--- src/update.rs
+++ src/update.rs
@@ -1,51 +1,50 @@
 use crate::model::Model;
 use crate::text::{show_current_values, show_help_menu};
 use nannou::prelude::{App, Frame, Update};
+use rayon::prelude::*;
 
 // Update the state of our application every frame
 pub fn update(_app: &App, model: &mut Model, _update: Update) {
-    // TODO: Still pretty bad - .clone() is very expensive
-    let mut temp_flock = model.flock.clone();
-    for boid in &mut temp_flock {
-        boid.flock(&model.flock, model.win_rect);
-    }
-    model.flock = temp_flock;
+    // Create a temp flock, to ensure thread safety, so that the actual flock is not getting
+    // modified *and* compared to at the same time
+    let temp_flock = model.flock.clone();
+    model
+        .flock
+        .par_iter_mut()
+        .for_each(|boid| boid.flock(&temp_flock, model.win_rect));
 }
 
 // Draw our stuff to the screen every frame
 pub fn view(app: &App, model: &Model, frame: Frame) {
-    // Allows us to draw
-    let draw = app.draw();
-
     // Draw background
-    draw.background().rgb(0.1569, 0.1569, 0.1569);
+    model.draw.background().rgb(0.1569, 0.1569, 0.1569);
 
     // Only highlight the first boid, if it exists
     if model.keybinds.highlight_first {
         if let Some(boid) = model.flock.first() {
-            boid.show_perception(&draw, 0.025)
+            boid.show_perception(&model.draw, 0.025)
         }
     }
 
     // Show all the boids
     for boid in &model.flock {
-        boid.show(&draw);
+        boid.show(&model.draw);
         if model.keybinds.highlight_all {
-            boid.show_perception(&draw, 0.0025);
+            boid.show_perception(&model.draw, 0.0025);
         }
     }
 
     // Draw the help menu
     if model.keybinds.show_help_menu {
-        show_help_menu(&draw, model.win_rect);
+        show_help_menu(&model.draw, model.win_rect);
     }
 
     // Draw the current values
     if model.keybinds.show_current_values {
-        show_current_values(&draw, model.win_rect, model);
+        show_current_values(&model.draw, model.win_rect, model);
     }
 
     // Push stuff to screen
-    draw.to_frame(app, &frame)
+    model.draw.to_frame(app, &frame)
         .expect("Unable to draw to the frame");
 }
