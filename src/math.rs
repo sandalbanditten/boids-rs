a2b2706c529be6c5ed7086b2a6e61604af160673
sandalbanditten <sandalbanditten@tutanota.com>
2022-05-06 19:09:28 +0200

    Made the map function generic

--- src/math.rs
+++ src/math.rs
@@ -1,5 +1,9 @@
+use std::ops::{Add, Div, Mul, Sub};
 // Map a number within a range, to within another range
-pub fn map(num: f32, start_one: f32, stop_one: f32, start_two: f32, stop_two: f32) -> f32 {
+pub fn map<T>(num: T, start_one: T, stop_one: T, start_two: T, stop_two: T) -> T
+where
+    T: Mul<Output = T> + Div<Output = T> + Add<Output = T> + Sub<Output = T> + Copy,
+{
     // Return the mapped result - copy pasted from the p5js reference
     (num - start_one) / (stop_one - start_one) * (stop_two - start_two) + start_two
 }
